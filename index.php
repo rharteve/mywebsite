ef9aac628700729bed878b1cfa3a93393f9ba3e8
Robert Hartevelt <Robert.Hartevelt@planet.nl>
2019-03-20 10:11:15 +0100

    eerste index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+
+
+
+<?php
+   printf("<title> RoHa WebPage test </title>");
+   printf("<body> ");
+   $hostnaam=gethostname();
+   printf("Ok de hostname is hier $hostnaam \n");
+
+   printf("</body> ");
+?>
